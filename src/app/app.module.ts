48da4bc1f13ebcda605c8e4901ab5134b35c4a7c
Ana Leite <silvaaraujoleite@gmail.com>
2018-05-22 19:33:52 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
+//import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponente2Component
+	//MeuComponenteComponent
   ],
   imports: [
     BrowserModule
