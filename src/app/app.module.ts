1d6aaa38adf92627ed26234f8748643ffd5586ca
Karen Vicente <karenyasmin37@gmail.com>
2018-05-22 22:24:57 -0300

    add navbar e pagina de cadastro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DataTableModule,ButtonModule , InputTextModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 //import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuComponente2Component
+    MeuComponente2Component,
+    NavbarComponent,
+    UsuarioFormComponent
 	//MeuComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    DataTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usuario-form/usuario-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-usuario-form',
+  templateUrl: './usuario-form.component.html',
+  styleUrls: ['./usuario-form.component.css']
+})
+export class UsuarioFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
